044a673c3dd8424e8bd64af30405dd8c4b2042cb
diabhey <abhimanyuselvan@pm.me>
2024-11-02 00:54:51 +0530

    feat: iniital e2e

---
+++ speech-ai/clients/sts.md
@@ -0,0 +1,5 @@
+# Speech-to-Speech
+
+```bash
+python3 sts.py --asr-server 184.105.190.88:50051 --tts-server 184.105.190.88:50052 --language-code en-US --input-device 1 --output-device 2 --output output.wav --stream
+```
--- speech-ai/scripts/tts.sh
+++ speech-ai/scripts/tts.sh
@@ -14,5 +14,16 @@ docker run -it --rm --name=$CONTAINER_NAME \
   -p 50052:50052 \
   nvcr.io/nim/nvidia/fastpitch-hifigan-tts:1.0.0
 
-  python3 clients/talk.py --server 184.105.190.88:50052 --text "Hello, this is a speech synthesizer." --language-code en-US --output output.wav
-  python3 clients/talk.py --server 184.105.190.88:50052 --text "Hello, this is a speech synthesizer." --language-code en-US --output-device 2 --stream
+python3 clients/talk.py --server <public-ip>:50052 \
+  --text \
+  "Now available for every DigitalOcean user,
+  GPU Droplets are powered by NVIDIA H100 GPUs, which are one of the most powerful computers accessible today,
+  and feature 640 Tensor Cores and 128 Ray Tracing Cores, facilitating high-speed data processing. 
+  GPU Droplets offer on-demand access to these machines, enabling developers, startups, and innovators to train AI models, 
+  process large datasets, and handle complex neural networks with ease." \
+  --language-code en-US \
+  --output-device 1 --stream
+
+
+
+  output output.wav
